dbd8d390c0466affce34cce83477f5736ed0c70d
neilcantorne <cantorn.neil@gmail.com>
2023-04-24 01:45:12 +0800

    update root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { Calculator } from "./components/Calculator";
 
 export function App() {
-    return <div></div>;
+    return <div>
+        <h2>Convolutional Network Shape Calculator</h2>
+        <Calculator></Calculator>
+    </div>
 }
